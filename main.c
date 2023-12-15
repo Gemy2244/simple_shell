ae9a89c6cb62303545d5b32d9aaa445238ee4cf8
Gemy2244 <me@example.com>
2023-12-15 09:35:46 -0800

    simple shell project

--- main.c
+++ main.c
@@ -1,103 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include "main.h"
+#include "shell.h"
 
-#define MAX_LINE 128
-#define MAX_ARGS 32
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: on success 0, otherwise 1
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-int main(int argc, char *argv[]) {
-  char line[MAX_LINE];
-  char *args[MAX_ARGS];
-  char *path_env = getenv("PATH");
-  int ret;
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-  // Display prompt
-  printf("> ");
-
-  while (fgets(line, sizeof(line), stdin)) {
-    // Remove trailing newline
-    strtok(line, "\n");
-
-    // Check for end of file
-    if (feof(stdin)) {
-      printf("\n");
-      break;
-    }
-
-    // Check for built-in commands
-    if ((ret = execute_builtin(line)) != 0) {
-      if (ret == -1) {
-        fprintf(stderr, "Error: unknown command\n");
-      }
-      continue;
-    }
-
-    // Parse arguments
-    int arg_count = 0;
-    char *token = strtok(line, " ");
-    while (token && arg_count < MAX_ARGS - 1) {
-      args[arg_count++] = token;
-      token = strtok(NULL, " ");
-    }
-
-    // Check for exceeding argument limit
-    if (token) {
-      printf("Error: too many arguments\n");
-      continue;
-    }
-
-    args[arg_count] = NULL;
-
-    // Check if command exists in the current directory
-    struct stat statbuf;
-    if (stat(args[0], &statbuf) == 0 && S_ISREG(statbuf.st_mode)) {
-      // Execute command directly
-      ret = fork();
-      if (ret == -1) {
-        perror("fork");
-        exit(1);
-      }
-    } else {
-      // Search for command in the PATH
-      char *path_token = strtok(path_env, ":");
-      while (path_token) {
-        char full_path[MAX_LINE];
-        sprintf(full_path, "%s/%s", path_token, args[0]);
-        if (stat(full_path, &statbuf) == 0 && S_ISREG(statbuf.st_mode)) {
-          // Found command, update args[0] and execute
-          args[0] = full_path;
-          ret = fork();
-          if (ret == -1) {
-            perror("fork");
-            exit(1);
-          }
-          break;
-        }
-        path_token = strtok(NULL, ":");
-      }
-    }
-
-    if (ret == -1) {
-      perror("fork");
-      exit(1);
-    }
-
-    if (ret == 0) {
-      // Child process
-      execvp(args[0], args);
-      perror("execvp");
-      exit(EXEC_ERROR);
-    } else {
-      // Parent process
-      wait(NULL);
-    }
-
-    // Display prompt again
-    printf("> ");
-  }
-
-  return 0;
-}	
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
