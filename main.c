9ab4e143d32564dfd221c65e7a9702be018ac641
Gemy2244 <me@example.com>
2023-12-15 04:00:33 -0800

    simple shell project

---
+++ main.c
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+void print_array(int *array, size_t size)
+
+#endif	
